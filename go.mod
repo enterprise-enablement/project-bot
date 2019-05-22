fd49bd778fdcfa41b52fb73d46dc7f5abc7c840d
phenry725 <paulh725@gmail.com>
2019-05-22 16:01:46 -0700

    add more placeholder code to import and use a github access token

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module github.com/phenry725/project-gh-bot
 
 go 1.12
 
-require github.com/google/go-github/v25 v25.0.4
+require (
+	github.com/google/go-github/v25 v25.0.4
+	github.com/pkg/errors v0.8.1 // indirect
+	github.com/stretchr/testify v1.3.0 // indirect
+	go.uber.org/atomic v1.4.0 // indirect
+	go.uber.org/multierr v1.1.0 // indirect
+	go.uber.org/zap v1.10.0
+	golang.org/x/oauth2 v0.0.0-20180821212333-d2e6202438be
+)
--- main.go
+++ main.go
@@ -1,27 +1,52 @@
 package main
 
 import (
-  "fmt"
-  "github.com/google/go-github/v25/github"
-  "context"
+	"context"
+	"fmt"
+	"github.com/google/go-github/v25/github"
+	"go.uber.org/zap"
+	"golang.org/x/oauth2"
+	"os"
+	"sync"
 )
 
 var (
+	githubAccessToken string
 
+	prodLogger *zap.Logger
+	logger     zap.SugaredLogger
+	once       sync.Once
 )
 
-const (
+const ()
 
-)
+func init() {
+	var set bool
+	once.Do(func() {
+		prodLogger, _ = zap.NewDevelopment()
+		logger = *prodLogger.Sugar()
+		defer logger.Sync()
+		githubAccessToken, set = os.LookupEnv("GITHUB_ACCESS_TOKEN")
+		if !set {
+			logger.Fatalw("Token environment variable not set",
+				"envVarToSet", "GITHUB_ACCESS_TOKEN",
+			)
+		}
+	})
+}
 
 func main() {
-  client := github.NewClient(nil)
-  opt := &github.RepositoryListByOrgOptions{Type: "public"}
-  repos, _, err := client.Repositories.ListByOrg(context.Background(), "github", opt)
-  if err != nil {
-    panic(err)
-  }
-  for _, repo := range repos {
-    fmt.Println(repo)
-  }
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: githubAccessToken},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	repos, _, err := client.Repositories.List(context.Background(), "", nil)
+	if err != nil {
+		panic(err)
+	}
+	for _, repo := range repos {
+		fmt.Println(repo)
+	}
 }
